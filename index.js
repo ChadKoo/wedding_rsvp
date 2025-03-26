ec2c09f023399ba602484bc467e90c65a2d6d1fc
Chadwick Koo <chadwickkoo@outlook.com>
2025-03-26 19:15:32 +0300

    Modified styles file

---
+++ index.js
@@ -0,0 +1,5 @@
+const rsvpBtn = document.getElementById('rsvpBtn');
+const attendeesList = document.getElementById('attendees');
+const firstNameInput = document.querySelector('input[placeholder="First Name, Last Name"]');
+const addressInput = document.querySelector('input[placeholder="Address"]');
+const phoneInput = document.querySelector('input[placeholder="Phone Number"]');
